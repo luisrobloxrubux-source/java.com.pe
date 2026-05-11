b79b56541d4f2974d015d5a6f0270b9b81dc69e5
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-05-11 10:02:46 -0500

    Add files via upload

---
+++ Concatenacion.java
@@ -0,0 +1,12 @@
+public class Concatenacion {
+
+    public static void main(String[] args) {
+
+        String nombre = "John";
+        String primerApellido = "Doe";
+
+        String nombreConApellido = nombre + " " + primerApellido;
+
+        System.out.println(nombreConApellido);
+    }
+}
