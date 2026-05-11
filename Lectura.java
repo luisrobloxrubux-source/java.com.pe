9f2cdca5c2d0f5176a40868638c3f96326f9c3b2
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-05-11 10:08:53 -0500

    Add files via upload

---
+++ Lectura.java
@@ -0,0 +1,19 @@
+public class Lectura {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingrese un valor entero: ");
+        int numero1 = entrada.nextInt();
+
+        System.out.print("Ingrese otro valor entero: ");
+        int numero2 = entrada.nextInt();
+
+        int suma = numero1 + numero2;
+
+        System.out.println("La suma es " + suma);
+
+        entrada.close();
+    }
+}
