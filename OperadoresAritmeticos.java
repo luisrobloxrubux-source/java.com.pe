32669df06c1d263f7b3e60952f209db3a5b9dd50
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-05-11 10:08:23 -0500

    Add files via upload

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,16 @@
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        int numero1 = 7;
+        int numero2 = 5;
+
+        System.out.println("Operaciones con \"numero1\" y \"numero2\":");
+
+        System.out.println("Suma: " + (numero1 + numero2));
+        System.out.println("Resta: " + (numero1 - numero2));
+        System.out.println("Multiplicación: " + (numero1 * numero2));
+        System.out.println("División: " + (numero1 / numero2));
+        System.out.println("Módulo: " + (numero1 % numero2));
+    }
+}
