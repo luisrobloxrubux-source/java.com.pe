e152a0c79ac9b248de1ea45a89391bd99e9b2758
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-05-12 19:37:14 -0500

    Add files via upload

---
+++ import java.util.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Lectura {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print(" ");
+        int numero1 = 10;
+
+        System.out.print(" ");
+        int numero2 = 20;
+
+        int suma = numero1 + numero2; // 10 + 20
+        System.out.println("La suma es " + suma);
+    }
+}
