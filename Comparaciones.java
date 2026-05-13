7eff9c18df61b6ff3b2997865ffca867b522eabb
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-05-12 19:50:48 -0500

    Add files via upload

---
+++ Comparaciones.java
@@ -0,0 +1,24 @@
+public class Comparaciones {
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 7;
+
+        System.out.print(numero1 + " es mayor a " + numero2 + ": ");
+        System.out.println(numero1 > numero2);
+
+        System.out.print(numero1 + " es menor a " + numero2 + ": ");
+        System.out.println(numero1 < numero2);
+
+        System.out.print(numero1 + " es mayor o igual a " + numero2 + ": ");
+        System.out.println(numero1 >= numero2);
+
+        System.out.print(numero1 + " es menor o igual a " + numero2 + ": ");
+        System.out.println(numero1 <= numero2);
+
+        System.out.print(numero1 + " es igual a " + numero2 + ": ");
+        System.out.println(numero1 == numero2);
+
+        System.out.print(numero1 + " no es igual a " + numero2 + ": ");
+        System.out.println(numero1 != numero2);
+    }
+}
